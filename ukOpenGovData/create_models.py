46199c058765d4fb02b739965fa66ff39168b464
Siddesh Iyer <siddesh.iyer@sportfin.io>
2023-08-19 23:18:08 +0100

    fix create database command

--- ukOpenGovData/create_models.py
+++ ukOpenGovData/create_models.py
@@ -8,25 +8,27 @@ import sqlite3
 import logging
 
 
-def create_db(csv_filename, db_dir, app_name, month=None):
-    static_dir = STATIC_URL + app_name + "/"
-    model_name = csv_filename.split(static_dir)[1][:-4]
+def create_db(csv_filename, db_dir, app_name):
     if not os.path.exists(db_dir):
         os.makedirs(db_dir)
 
-    if not month:
-        db_name = app_name
-    else:
-        db_name = app_name + "-" + month
-        model_name = model_name.split(f"{month}\\")[1]
-
-    db_path = os.path.join(db_dir, str(db_name + ".db"))
+    db_path = os.path.join(db_dir, str(app_name + ".db"))
 
     df = pd.read_csv(csv_filename)
+    if "policeData" in app_name:
+        df_grouped = df.groupby("Crime type")
 
-    logging.info(f"Adding table {model_name} to {db_path}")
-    conn = sqlite3.connect(db_path)
-    df.to_sql(model_name, conn, if_exists="replace", index=True)
+        logging.info(f"Adding db to {db_path}")
+        conn = sqlite3.connect(db_path)
+
+        for group_name, grouped_df in df_grouped:
+            model_name = group_name.upper().replace(" ", "")
+            logging.info(f"Adding table {model_name} to {db_path}")
+            grouped_df.to_sql(model_name, conn, if_exists="append", index=True)
+    else:
+        logging.info(f"Adding table {model_name} to {db_path}")
+        conn = sqlite3.connect(db_path)
+        df.to_sql(model_name, conn, if_exists="replace", index=True)
 
     # Close the connection
     conn.close()
--- ukOpenGovData/management/commands/import_all_csv.py
+++ ukOpenGovData/management/commands/import_all_csv.py
@@ -1,8 +1,16 @@
 import os
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from tqdm import tqdm
 from ukOpenGovData.create_models import create_db
 from django.core.management import BaseCommand
-from ukOpenGovData.settings import STATIC_URL, BASE_DIR
+from ukOpenGovData.settings import (
+    STATIC_URL,
+    BASE_DIR,
+    S3_CLIENT,
+    S3_BUCKET_NAME,
+    S3_BUCKET_URL,
+)
 import logging
 
 separator = "-" * 40
@@ -22,37 +30,52 @@ class Command(BaseCommand):
         logging.info(separator)
         logging.info(f"Creating database for {app_name}")
         logging.info(separator)
-        if app_name == "activeData" or app_name == "ONS":
+        if app_name == "policeData":
+            start_date = datetime.strptime("2020-07", "%Y-%m")
+            end_date = datetime.strptime("2023-06", "%Y-%m")
+            months = []
+            while start_date <= end_date:
+                months.append(datetime.strftime(start_date, "%Y-%m"))
+                start_date += relativedelta(months=1)
+            for month in tqdm(months, position=0, desc="Progress: "):
+                month_dir = static_dir + month
+                # List objects in month dir
+                response = S3_CLIENT.list_objects_v2(
+                    Bucket=S3_BUCKET_NAME, Prefix=month_dir
+                )
+                # Extract CSVs filenames
+                csv_filenames = [
+                    obj["Key"]
+                    for obj in response.get("Contents", [])
+                    if obj["Key"].endswith(".csv")
+                ]
+                logging.info(f"CSV Files Identified for {month}:")
+                logging.info(csv_filenames)
+                db_dir = os.path.join(BASE_DIR, "databases", app_name, month)
+                for csv in tqdm(
+                    csv_filenames, desc=f"Creating databases from CSVs for {month}"
+                ):
+                    csv_path = S3_BUCKET_URL + csv
+                    create_db(csv_path, db_dir, f"{app_name}-{month}")
+
+        else:
+            # List objects in the S3 bucket with the specified prefix
+            response = S3_CLIENT.list_objects_v2(
+                Bucket=S3_BUCKET_NAME, Prefix=static_dir
+            )
+
+            # Extract CSV filenames from the S3 response
             csv_filenames = [
-                filename
-                for filename in os.listdir(static_dir)
-                if filename.endswith(".csv")
+                obj["Key"]
+                for obj in response.get("Contents", [])
+                if obj["Key"].endswith(".csv")
             ]
+
             logging.info("CSV Files Identified:")
             logging.info(csv_filenames)
             db_dir = os.path.join(BASE_DIR, "databases", app_name)
             for csv in tqdm(csv_filenames, desc="Creating databases from CSVs"):
-                csv_path = os.path.join(static_dir, csv)
+                csv_path = S3_BUCKET_URL + csv
                 create_db(csv_path, db_dir, app_name)
-
-        elif app_name == "policeData":
-            month_dirs = os.listdir(static_dir)
-            for month in month_dirs:
-                month_dir = os.path.join(static_dir, month)
-                csv_filenames = [
-                    filename
-                    for filename in os.listdir(month_dir)
-                    if filename.endswith(".csv")
-                ]
-
-                db_dir = os.path.join(BASE_DIR, "databases", app_name, month)
-                logging.info(f"Creating databases for {month} at {db_dir}")
-                logging.info("CSV Files Identified:")
-                logging.info(csv_filenames)
-                for csv in tqdm(
-                    csv_filenames, desc=f"Creating police database for {month}"
-                ):
-                    csv_path = os.path.join(month_dir, csv)
-                    create_db(csv_path, db_dir, app_name, month=month)
 
         print("Creation logs in", os.path.join(BASE_DIR, "db_create.log"))
